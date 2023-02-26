a58a816225fe319907ebe30245391bf390ae00e1
Lukic <bojan.lukic@dlr.de>
2023-02-26 17:14:58 +0100

    Change loading page background color, remove min.map reference

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("body").queryLoader2({
-        barColor: "#6e6d73",
+        barColor: "#FFFFFF",
         backgroundColor: "#02356a",
         percentage: true,
         barHeight: 1,
